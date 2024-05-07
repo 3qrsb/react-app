35afb8f2d00313c5058b80e4c5cbb6acda3e6dd2
Yers <eabuov4@gmail.com>
2024-05-07 21:56:12 +0500

    expense list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
-import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "aabba", amount: 12, category: "Utilities" },
+    { id: 3, description: "acacaa", amount: 1330, category: "Utilities" },
+    { id: 4, description: "dddd", amount: 110, category: "Utilities" },
+  ]);
+
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
