757d063d7688ce4ec92b0aca9b489cae14d65c09
Yers <eabuov4@gmail.com>
2024-05-08 17:49:24 +0500

    react hook form and zod

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -18,6 +20,7 @@ function App() {
 
   return (
     <div>
+      <div className="mb-5">Expense Form</div>
       <div className="mb-3">
         <ExpenseFilter
           onSelectCategory={(category) => setSelectedCategory(category)}
